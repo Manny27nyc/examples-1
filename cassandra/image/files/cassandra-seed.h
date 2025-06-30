dea690471b48abfc27f3e9e0a0b08de1685b6ef9
Manny27nyc <Fordamboy1@gmail.com>
2025-06-30 16:50:34 -0400

    🔒 Inject minimal licensing header (automated)

--- cassandra/image/files/cassandra-seed.h
+++ cassandra/image/files/cassandra-seed.h
@@ -1,3 +1,4 @@
+// © Licensed Authorship: Manuel J. Nieves (See LICENSE for terms)
 /* Created by "go tool cgo" - DO NOT EDIT. */
 
 /* package command-line-arguments */
--- guestbook-go/public/script.js
+++ guestbook-go/public/script.js
@@ -1,3 +1,4 @@
+// © Licensed Authorship: Manuel J. Nieves (See LICENSE for terms)
 $(document).ready(function() {
   var headerTitleElement = $("#header h1");
   var entriesElement = $("#guestbook-entries");
